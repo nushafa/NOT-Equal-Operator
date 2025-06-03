66e3e96b88e336ce4f7b3cbd6e085571b5ab1e95
Abiat Nushafa <nushafa1@gmail.com>
2025-06-03 20:50:24 +0600

    Add files via upload

---
+++ NOT Equal Operator.py
@@ -0,0 +1,26 @@
+a = 10
+b = 12
+c = 12
+
+print (a != b)
+print (b != c)
+
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a, 'and', b, 'are not equal')
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print("Hello")
+
+a = int(input("Enter a number: "))
+
+if a%2 != 0:
+    print(a, "is not an even number")
+else:
+    print(a, "is an even number")
